958871f3ffd182ae95b286698b996e7bd58425e2
T-manDon <opiyoadiema@gmail.com>
2023-07-24 18:20:47 -0700

    printf

---
+++ utils.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * is_printable - determines printability of a variable
+ * @c: Char variable
+ *
+ * Return: 1 Success else 0
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Appends the code
+ * @buffer: buffer array
+ * @i: Index variable
+ * @ascii_code: ASCII cod
+ *
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - checks if char is digit
+ * @c: Char var
+ *
+ * Return: 1 success, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Changes the size number
+ * @num: Number variable
+ * @size: size variable
+ *
+ * Return: variable outcome
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Chnages the unsigned
+ * @num: Number variable
+ * @size: Size variable
+ *
+ * Return: resulting value
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
